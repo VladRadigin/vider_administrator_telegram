7bac61004d6a312146d83777981ccba9879bea0d
VladRadygin <angry.gamer.comi@gmail.com>
2022-04-16 16:49:16 +0200

    Adding logging for the bot

--- bot.py
+++ bot.py
@@ -1,19 +1,28 @@
 from telegram import Update, Bot
 from telegram.ext import Updater, MessageHandler, Filters, CallbackContext, CommandHandler
 from telegram.utils.request import Request
+import logging
 
 
 token = '5148676833:AAGv1DO8rqIHwarcdna9W6OwH1RNytMMP1c'
 
 
+# Реализация логирования в отдельный файл: bot.log
+logging.basicConfig( format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log' )
+
+
 # Реагирование бота на команду: /start
 def start(update: Update, context: CallbackContext):
     text = 'Pressed: /start'
-    print(text)
+    logging.info(text)
     update.message.reply_text(text) # Самый простой способ ответить пользователю
 
 def main():
     bot = Updater(token)
+
+    logging.info('Bot is working...')
 
     dp = bot.dispatcher
     dp.add_handler(CommandHandler('start', start))
